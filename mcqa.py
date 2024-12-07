6bb2b82abb8a6a5075ab2ac9e58c62713b0c9d63
sahraslet <sahra220297@gmail.com>
2024-12-08 00:01:18 +0100

    'update files for multiple choice evaluation'

--- mcqa.py
+++ mcqa.py
@@ -1,9 +1,15 @@
 import torch
-from torch.utils.data import DataLoader
-from transformers import PreTrainedTokenizerFast, BertForMultipleChoice, BertTokenizer
-from sklearn.metrics import accuracy_score
+from torch.utils.data import Subset
 from torch.utils.data import Dataset
+from models.bert_model import BertForMaskedLM, BertModel, BertForMultipleChoice
+from sklearn.model_selection import train_test_split
 import pandas as pd
+from transformers import Trainer, TrainingArguments
+from sklearn.metrics import accuracy_score, f1_score
+from transformers import PreTrainedTokenizerFast
+
+
+
 
 class MCQADataset(Dataset):
 
@@ -23,16 +29,9 @@ class MCQADataset(Dataset):
         options = self.dataset.loc[idx, ['opa', 'opb', 'opc', 'opd']].values
         label = self.dataset.loc[idx, 'cop'] - 1
 
-        # Tokenize
         encodings = []
         for option in options:
-            if self.use_context:
-                # Combine context, question and option
-                text = f"{context} {question} {option}"
-            else:
-                # Combine question and option only
-                text = f"{question} {option}"
-
+            text = f"{context} {question} {option}" if self.use_context else f"{question} {option}"
             encoding = self.tokenizer(
                 text,
                 truncation=True,
@@ -42,52 +41,75 @@ class MCQADataset(Dataset):
             )
             encodings.append(encoding)
 
-        # Stack the different encodings into a batch (in this case with 4 options)
-        input_ids = torch.cat([e['input_ids'] for e in encodings], dim=0)
-        attention_mask = torch.cat([e['attention_mask'] for e in encodings], dim=0)
+        input_ids = torch.stack([e['input_ids'].squeeze(0) for e in encodings])  # [4, max_length]
+        attention_mask = torch.stack([e['attention_mask'].squeeze(0) for e in encodings])  # [4, max_length]
 
-        return input_ids, attention_mask, label
+        # Rückgabe als Dictionary
+        return {
+            "input_ids": input_ids,
+            "attention_mask": attention_mask,
+            "labels": torch.tensor(label, dtype=torch.long)
+        }
 
-# Load BERT-Tokenizer and pretrained model
-tokenizer = PreTrainedTokenizerFast.from_pretrained('checkpoints/checkpoint-220000')
-model = BertForMultipleChoice.from_pretrained('checkpoints/checkpoint-220000')
-
-# Set the model to evaluation mode
-model.eval()
-
-# Load our dataset
-dataset = MCQADataset(csv_path='medmcqa_data.csv', tokenizer=tokenizer, use_context=True)
-
-# DataLoader
-data_loader = DataLoader(dataset, batch_size=16, shuffle=False)
-
-def evaluate(model, data_loader):
-    all_preds = []
-    all_labels = []
-
-    with torch.no_grad():
-        for input_ids, attention_mask, labels in data_loader:
-            input_ids = input_ids.cuda() if torch.cuda.is_available() else input_ids
-            attention_mask = attention_mask.cuda() if torch.cuda.is_available() else attention_mask
-            labels = labels.cuda() if torch.cuda.is_available() else labels
-
-            # Forward Pass
-            outputs = model(input_ids=input_ids, attention_mask=attention_mask)
-            logits = outputs.logits
-
-            # Predictions
-            preds = torch.argmax(logits, dim=1)
-
-            # Save predictions
-            all_preds.extend(preds.cpu().numpy())
-            all_labels.extend(labels.cpu().numpy())
-
-    # Calculate accuracy
-    accuracy = accuracy_score(all_labels, all_preds)
-    return accuracy
-
-accuracy = evaluate(model, data_loader)
-print(f'Accuracy: {accuracy * 100:.2f}%')
+max_length = 256
+model_name_or_path = 'checkpoints/checkpoint-220000'
+tokenizer = PreTrainedTokenizerFast.from_pretrained(model_name_or_path)
 
 
+def compute_metrics(pred):
+    preds = pred.predictions[0] if isinstance(pred.predictions, tuple) else pred.predictions
+    labels = pred.label_ids
+    preds = preds.argmax(-1)
+    f1 = f1_score(labels, preds, average='micro')
+    acc = accuracy_score(labels, preds)
+    return {
+        'f1': f1,
+        'acc': acc
+    }
 
+
+if __name__ == '__main__':
+
+    dataset_name = 'medmcqa_data.csv'
+    dataset = MCQADataset(csv_path=dataset_name, tokenizer=tokenizer, use_context=False)
+
+    indices = list(range(len(dataset)))
+    train_indices, temp_indices = train_test_split(indices, test_size=0.2, random_state=42)
+    val_indices, test_indices = train_test_split(temp_indices, test_size=0.5, random_state=42)
+
+    train_dataset = Subset(dataset, train_indices)
+    val_dataset = Subset(dataset, val_indices)
+    test_dataset = Subset(dataset, test_indices)
+
+    model = BertForMultipleChoice.from_pretrained(model_name_or_path, num_labels=5)
+
+    # frozen bert encoder
+    for param in model.bert.parameters():
+        param.requires_grad = False
+
+    training_args = TrainingArguments(
+        output_dir='./classification_checkpoint',  # output directory
+        num_train_epochs=10,  # total number of training epochs
+        per_device_train_batch_size=64,  # batch size per device during training
+        per_device_eval_batch_size=64,  # batch size for evaluation
+        warmup_steps=0,  # number of warmup steps for learning rate scheduler
+        weight_decay=0.01,  # strength of weight decay
+        logging_dir='./logs',
+        report_to='tensorboard',  # directory for storing logs
+        load_best_model_at_end=True,  # load the best model when finished training (default metric is loss)
+        metric_for_best_model='f1',
+        # but you can specify `metric_for_best_model` argument to change to accuracy or other metric
+        logging_steps=100,  # log & save weights each logging_steps
+        save_strategy="epoch",
+        evaluation_strategy="epoch",# evaluate each `logging_steps`
+    )
+
+    trainer = Trainer(
+        model=model,  # the instantiated Transformers model to be trained
+        args=training_args,  # training arguments, defined above
+        train_dataset=train_dataset,  # training dataset
+        eval_dataset=val_dataset,  # evaluation dataset
+        compute_metrics=compute_metrics,  # the callback that computes metrics of interest
+    )
+    trainer.train()
+    print(trainer.evaluate(eval_dataset=test_dataset))
--- models/bert_model.py
+++ models/bert_model.py
@@ -2,6 +2,7 @@ import os
 from functools import partial
 import torch
 import torch.nn as nn
+from torch.nn import CrossEntropyLoss
 from transformers.modeling_utils import unwrap_model
 from safetensors.torch import load_file as safe_load_file
 from models.bert_config import BertConfig
@@ -162,5 +163,66 @@ class BertForSequenceClassification(BaseModel):
         return MaskedLMOutput(
             loss=loss,
             logits=logits,
+            hidden_states=outputs.hidden_states,
+        )
+class BertForMultipleChoice(BaseModel):
+    config_class = BertConfig  # Define config class for Multiple Choice
+
+    def __init__(self, config):
+        super(BertForMultipleChoice, self).__init__()
+        self.config = config
+        self.bert = BertModel(config)
+        classifier_dropout = (
+            config.classifier_dropout if config.classifier_dropout is not None else config.hidden_dropout_prob
+        )
+        self.dropout = nn.Dropout(classifier_dropout)
+        self.classifier = nn.Linear(config.hidden_size, 1)
+
+    def forward(
+            self,
+            input_ids: Optional[torch.Tensor] = None,
+            attention_mask: Optional[torch.Tensor] = None,
+            position_ids: Optional[torch.Tensor] = None,
+            inputs_embeds: Optional[torch.Tensor] = None,
+            labels: Optional[torch.Tensor] = None,
+            return_dict: Optional[bool] = None,
+    ) -> Union[Tuple[torch.Tensor], MaskedLMOutput]:
+        r"""
+        labels (`torch.LongTensor` of shape `(batch_size,)`, *optional*):
+            Labels for computing the multiple choice classification loss. Indices should be in `[0, ...,
+            num_choices-1]` where `num_choices` is the size of the second dimension of the input tensors. (See
+            `input_ids` above)
+        """
+        # return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+        num_choices = input_ids.shape[1] if input_ids is not None else inputs_embeds.shape[1]
+
+        input_ids = input_ids.view(-1, input_ids.size(-1)) if input_ids is not None else None
+        attention_mask = attention_mask.view(-1, attention_mask.size(-1)) if attention_mask is not None else None
+        position_ids = position_ids.view(-1, position_ids.size(-1)) if position_ids is not None else None
+
+        outputs = self.bert(
+            input_ids,
+            attention_mask=attention_mask,
+            position_ids=position_ids,
+        )
+
+        pooled_output = outputs.last_hidden_state[:, 0, :]
+
+        pooled_output = self.dropout(pooled_output)
+        logits = self.classifier(pooled_output)
+        reshaped_logits = logits.view(-1, num_choices)
+
+        loss = None
+        if labels is not None:
+            loss_fct = CrossEntropyLoss()
+            loss = loss_fct(reshaped_logits, labels)
+
+        if not return_dict:
+            output = (reshaped_logits, pooled_output)
+            return ((loss,) + output) if loss is not None else output
+
+        return MaskedLMOutput(
+            loss=loss,
+            logits=reshaped_logits,
             hidden_states=outputs.hidden_states,
         )
